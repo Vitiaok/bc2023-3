4ff2628c7e2e80c5325358bda4d61fef0d202346
Vitiaok <skrinikviktor96@gmail.com>
2023-10-02 09:40:16 +0300

    Changed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-fs.readFile('/Web-Server/lab3/data.json', (err, data) => {
+fs.readFile('/Web-Server/lab3/bc2023-3/data.json', (err, data) => {
 
   if (err === null) {
     console.log(data.toString());
@@ -12,11 +12,24 @@ fs.readFile('/Web-Server/lab3/data.json', (err, data) => {
     
   
   const object = JSON.parse(data);
-  let content = object[4].value;
-  let content1 = object[14].value;
+  let n =0;
+  while(object[n].id_api != "BS2_IncomeTotal")
+  {
+    n++;
+    
+  }
+  let content = (object[n].value).toString() ;
+  let k=0;
+  while(object[k].id_api != "BS2_ExpensesTotal")
+  {
+    k++;
+  }
+  let content1 = (object[k].value).toString();
+  //let content1 = object[14].value;
+  //const content of object;
   let finalcontent =(`Доходи, всього:${content}\nВитрати, всього:${content1}`);
   
-  fs.writeFile('/Web-Server/lab3/output.txt', finalcontent, err => {
+  fs.writeFile('/Web-Server/lab3/bc2023-3/output.txt', finalcontent, err => {
     if (err) {
       console.error(err);
     }
